a9fb904c005dc0190e61284fc2e5ed8bd7bfcdc3
psforza <psforza@redhat.com>
2017-01-03 21:04:42 +0100

    xx

--- salesVisualizer/geo-ws-rand/serverRand.js
+++ salesVisualizer/geo-ws-rand/serverRand.js
@@ -17,7 +17,6 @@ app.get('/', function(req, res){
 
 http.listen(serverPort, function(){
   console.log((new Date()) + ' [http.listen] listening on: ' + serverPort);
-  console.log((new Date()) + ' [io] listening at: ' + serverContext);
 });
 
 //--------------------------------\\
